fd05af0be9c82fa2a3409119685d07153f65eccd
Alexey Polovinka <apolovinka@correo.ugr.es>
2021-10-18 19:11:49 +0200

    #10 Created solution with User class

---
+++ GoSpeak/Model/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Model
+{
+    public class User
+    {
+        public int UserId {get;set;}
+        public int NumCorrectAnswers { get; set; }
+        public int NumWrongAnswers { get; set; }
+        public int[] AskedQuestion { get; set; }
+        public double Level { get; set; }
+    }
+}
