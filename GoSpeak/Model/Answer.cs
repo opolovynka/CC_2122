483861cc1cb906a8f91d6f8f6e60a4e1694697e3
Alexey Polovinka <apolovinka@correo.ugr.es>
2021-10-20 19:05:26 +0200

    closes #12

---
+++ GoSpeak/Model/Answer.cs
@@ -0,0 +1,10 @@
+﻿namespace Model
+{
+    public class Answer
+    {
+        public int Id { get; set; }
+        public string AnswerText { get; set; }
+        public bool IsCorrect { get; set; }
+        public bool QuestionID { get; set; }
+    }
+}
