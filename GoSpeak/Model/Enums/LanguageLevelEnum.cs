6590952793843d609411247b56101119a459c2e7
Alexey Polovinka <apolovinka@correo.ugr.es>
2021-11-17 19:04:12 +0100

    LanguageLevel enum for Question added

---
+++ GoSpeak/Model/Enums/LanguageLevelEnum.cs
@@ -0,0 +1,12 @@
+﻿namespace GoSpeak.Model.Enums
+{
+    public enum LanguageLevelEnum
+    {
+        A11 = 11,
+        A12 = 12,
+        B11 = 21,
+        B12 = 22,
+        C11 = 31,
+        C12 = 32
+    }
+}
