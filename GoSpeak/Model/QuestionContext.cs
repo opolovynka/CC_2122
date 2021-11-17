c7ba97c4ac4f092d50fd4a60f6bd05d0de34dd85
Alexey Polovinka <apolovinka@correo.ugr.es>
2021-11-17 17:54:34 +0100

    #25 : added context class to realize Question dataset access

---
+++ GoSpeak/Model/QuestionContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GoSpeak.Model
+{
+    public class QuestionContext : DbContext
+    {
+        public QuestionContext(DbContextOptions<QuestionContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Question> Questions { get; set; } = null!;
+    }
+}
