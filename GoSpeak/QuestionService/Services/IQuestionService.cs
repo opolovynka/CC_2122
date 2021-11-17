bfa29182a14f8c7e56d4700715ba0f3183ccea7b
Alexey Polovinka <apolovinka@correo.ugr.es>
2021-11-17 17:55:57 +0100

    #25: SOLID practices, implemented question service with interface for dependency ijection

---
+++ GoSpeak/QuestionService/Services/IQuestionService.cs
@@ -0,0 +1,12 @@
+﻿using GoSpeak.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GoSpeak.QuestionService.Services
+{
+    public interface IQuestionService
+    {
+        public Task<List<Question>> GetAllQuestions();
+        public Task<Question> GetQuestion(int userId, int questionId);
+    }
+}
---
+++ GoSpeak/QuestionService/Services/QuestionService.cs
@@ -0,0 +1,34 @@
+﻿using GoSpeak.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GoSpeak.QuestionService.Services
+{
+    public class QuestionService : IQuestionService
+    {
+        private QuestionContext _context;
+        public QuestionService(QuestionContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Question>> GetAllQuestions()
+        {
+            return await _context.Questions.Include(q => q.Answers).ToListAsync();
+        }
+
+        public async Task<Question> GetQuestion(int userId, int questionId = 0)
+        {
+            if (questionId == 0)
+            {
+                return await _context.Questions.Include(q => q.Answers).FirstAsync();
+            }
+            else
+            {
+                return await _context.Questions.Include(q => q.Answers).FirstAsync(q => q.Id != questionId);
+            }
+        }
+
+    }
+}
