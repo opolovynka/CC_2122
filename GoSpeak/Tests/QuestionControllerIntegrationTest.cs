2df5748352e975ff6b9a401934815e9d99f008bc
Alexey Polovinka <apolovinka@correo.ugr.es>
2021-11-24 19:22:56 +0100

    Added/fixed integration tests

--- GoSpeak/Tests/QuestionControllerIntegrationTest.cs
+++ GoSpeak/Tests/QuestionControllerIntegrationTest.cs
@@ -7,30 +7,47 @@ using GoSpeak.Model;
 using Xunit;
 using System.Collections.Generic;
 
-namespace Tests
+namespace GoSpeak.Tests
 {
     public class QuestionControllerIntegrationTest
     {
 
-        private ComponentConfig TestConfig { get; set; } = new ComponentConfig();
+        private ComponentConfig _testConfig { get; set; } = new ComponentConfig();
+        private string _endpoint = "api/question";
+        private string _url;
 
         public QuestionControllerIntegrationTest()
         {
-            TestConfig = new ConfigurationBuilder()
+            _testConfig = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json")
                 .Build()
                 .GetSection("Component")
                 .Get<ComponentConfig>();
+
+            if (String.IsNullOrEmpty(Environment.GetEnvironmentVariable("API_URL")))
+            {
+                _testConfig = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build()
+                .GetSection("Component")
+                .Get<ComponentConfig>();
+
+                _url = _testConfig.ServiceUri + _endpoint;
+            }
+            else
+            {
+                _url = Environment.GetEnvironmentVariable("API_URL") + _endpoint;
+            }
         }
+        
         [Fact]
+        [Trait("Test", "Integration")]
         public async Task Question_GetAllRequest_Success(){
-            //Arrange            
-            var httpClient = new HttpClient { BaseAddress = new Uri(TestConfig.ServiceUri) };
-            
-            string url = "api/question";
+            //Arrange
+            var httpClient = new HttpClient();            
 
             //Act
-            var response = await httpClient.GetAsync(url);
+            var response = await httpClient.GetAsync(_url);
             var responseJson = await response.EnsureSuccessStatusCode().Content.ReadAsStringAsync();
             var questions = JsonSerializer.Deserialize<List<Question>>(responseJson);
 
@@ -38,12 +55,13 @@ namespace Tests
         }
 
         [Fact]
+        [Trait("Test", "Integration")]
         public async Task Question_GetRequest_Success(){
             //Arrange            
-            var httpClient = new HttpClient { BaseAddress = new Uri(TestConfig.ServiceUri) };
+            var httpClient = new HttpClient();
             var userId = 1;
             var questionId = 1;
-            string url = "api/question" + "/" + userId + "/" + questionId;
+            string url = _url + "/" + userId + "/" + questionId;
 
             //Act
             var response = await httpClient.GetAsync(url);
@@ -54,19 +72,20 @@ namespace Tests
         }
 
         [Fact]
+        [Trait("Test", "Integration")]
         public async Task Question_GetRequest_BadRequest(){
             //Arrange
             int userId = 0;
-            var httpClient = new HttpClient { BaseAddress = new Uri(TestConfig.ServiceUri) };
+            var httpClient = new HttpClient();
             
-            string url = "api/question/" + userId;
+            string url = _url + "/" + userId;
 
             //Act and A
             var response = await httpClient.GetAsync(url);
             var result = response.Content.ReadAsStringAsync().Result;
 
             //Asserts
-            Assert.Equal(false, response.IsSuccessStatusCode);
+            Assert.False(response.IsSuccessStatusCode);
             Assert.Equal($"Parameter 'userId' is wrong. It should be more then 0", result);
         }
     }
