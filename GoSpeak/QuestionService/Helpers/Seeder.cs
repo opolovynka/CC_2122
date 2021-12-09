9d70054437f1985ff928166b5d247248f9455576
Alexey Polovinka <apolovinka@correo.ugr.es>
2021-12-09 19:41:46 +0100

    #28 added support for previouse version of language

--- GoSpeak/QuestionService/Helpers/Seeder.cs
+++ GoSpeak/QuestionService/Helpers/Seeder.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using System.Collections.Generic;
 using Microsoft.Extensions.DependencyInjection;
 using GoSpeak.Model;
-using System.Text.Json;
+using Newtonsoft.Json;
 
 public static class Seeder
 {
@@ -11,7 +11,7 @@ public static class Seeder
     public static void Seedit(string jsonData, IServiceProvider serviceProvider)
     {        
         List<Question> questions =
-         JsonSerializer.Deserialize<List<Question>>(
+         JsonConvert.DeserializeObject<List<Question>>(
            jsonData);
         using (
          var serviceScope = serviceProvider
